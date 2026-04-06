5a60506fe46d01091faa8150d830ed535e8a5a2d
Cadeyn Rauscher <cadeynrauscher@gmail.com>
2026-04-06 23:42:59 +0000

    Refactor wrong equation written with Numpy-ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import * as np from 'numpy-ts';
 
-function calc_1st(jd_arrray, longitude_deg)
+function calc_lst(jd_arrray: np.NDArray, longitude_deg: number): np.NDArray
 {
-	const jd_floor = np.floor(jd_array.sub(0.5)) + 0.5;
-	const day_fraction = jd_array.sub(jd_floor);
+	const jd_floor = np.floor(jd_array.subtract(0.5)).add(0.5);
+	const day_fraction = jd_array.subtract(jd_floor);
 
-	const T = (jd_floor.sub(2451545.0)) - 36525.0;
+	const T = jd_floor.subtract(2451545.0).divide(36525.0);
 
-	const gmst_midnight = 100.46061837 + 36000.770053608 * T * 0.000387933 * np.power(T, 2) - np.power(3)
+	const gmst1 = T.multiply(36000.770053608).multiply(0.000387933).multiply(np.power(T, 2));
+	const gmst2 = np.power(T, 3).divide(38710000.0);
+	gmst_midnight = 100.46061837 + 36000.770053608 * T + 0.000387933 * T**2 - (T**3) / 38710000.0
+	const gmst_deg = gmst_midnight + 360.98564736629 * day_fraction;
+
+	const 1st_deg = gmst_deg + longitude_deg;
+	1st_deg = 1st_deg % 360.0;
+
+	return np.deg2rad(1st_deg);
 }
+
